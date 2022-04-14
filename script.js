03e9a3c5109973642952f9e06ac0c0ae4a47cd3d
Dominic <design@digitalaccess.tech>
2022-04-14 09:07:36 +0200

    logic update, reveals and header img

--- script.js
+++ script.js
@@ -15,7 +15,24 @@ const config = {
         "Measure",
         "Optimise"
     ],
-    correct_text: "Well Done! That's correct!"
+    correct_text: "Well Done! That's correct!",
+    headerImg_on: false,
+    headerImg: "https://a.storyblok.com/f/112136/130x160/731788149f/avatar_matt-owen.png",
+    reveal_text_on: false,
+    reveal_text: "Reveal On",
+    reveal_img_on: true,
+    reveal_img: "https://a.storyblok.com/f/112136/509x186/ba5c52e287/pbemo.gif"
+}
+
+const instructionContainer = document.getElementById('instructionContainer')
+const image = document.getElementById('image')
+
+if (config.headerImg_on === true) {
+    image.src = config.headerImg
+    instructionContainer.style.paddingTop = '0.5rem'
+    instructionContainer.style.paddingBottom = '0'
+} else {
+    image.style.display = "none";
 }
 
 const instructions = document.getElementById('instructions')
@@ -82,9 +99,14 @@ function getDragAfterElement(container, y) {
     }).element
 }
 
+const reveal = document.getElementById('reveal')
+const revealText = document.getElementById('revealText')
+const revealImg = document.getElementById('revealImg')
+
+
 checkAnswer.textContent = 'Check Answer'
-
-
+revealText.textContent = config.reveal_text
+revealImg.src = config.reveal_img
 
 const checkAnswers = () => {
 
@@ -101,6 +123,22 @@ const checkAnswers = () => {
     if (answerOrder === checkArrangedOrder) {
         checkAnswer.textContent = "Well Done! That's correct!"
         checkAnswer.classList.add('correct')
+        if (config.reveal_text_on) {
+            reveal.style.display = 'flex'
+            revealText.style.display = 'flex'
+            revealText.style.opacity = '1'
+            instructionContainer.style.display = 'none'
+            container.style.display = 'none'
+        }
+
+        if (config.reveal_img_on) {
+            reveal.style.display = 'flex'
+            revealImg.style.display = 'flex'
+            revealImg.style.opacity = '1'
+            instructionContainer.style.display = 'none'
+            container.style.display = 'none'
+        }
+
     } else {
         checkAnswer.textContent = "❌Sorry, not yet"
         checkAnswer.classList.remove('correct')
@@ -111,5 +149,3 @@ const checkAnswers = () => {
 }
 
 checkAnswer.addEventListener('click', checkAnswers)
-
-const celebrationWindow = document.createElement('img')
